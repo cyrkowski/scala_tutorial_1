0b7c423acc7b2a4c344aa8c3d323d2613fc26dff
cyrkowski <mcyrkowski4@gmail.com>
2020-06-26 10:46:57 +0200

    code refactor

--- example/src/main/scala/example/Lists.scala
+++ example/src/main/scala/example/Lists.scala
@@ -22,19 +22,22 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-  def sumIter(xs: List[Int]): Int = {
-    var sum = 0
-    val xsIterator = xs.iterator
-    while(xsIterator.hasNext){
-      sum = sum + xsIterator.next
-    }
-    sum
-  }
+//  def sumIter(xs: List[Int]): Int = {
+//    var sum = 0
+//    val xsIterator = xs.iterator
+//    while(xsIterator.hasNext){
+//      sum = sum + xsIterator.next
+//    }
+//    sum
+//  }
 
   def sum(xs: List[Int]): Int = {
-    if(xs.isEmpty)return 0
-    var res = xs.head + sum(xs.tail)
-    res
+    if(xs.isEmpty){
+      0
+    }else {
+      val res = xs.head + sum(xs.tail)
+      res
+    }
   }
 
   /**
@@ -50,30 +53,31 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def maxIter(xs: List[Int]): Int = {
-    if(xs.isEmpty)throw new java.util.NoSuchElementException()
-
-    var max = Int.MinValue
-    val xsIterator = xs.iterator
-    while(xsIterator.hasNext){
-      val currentValue = xsIterator.next()
-      if(max < currentValue){
-        max = currentValue
-      }
-    }
-
-    max
-  }
+//  def maxIter(xs: List[Int]): Int = {
+//    if(xs.isEmpty)throw new java.util.NoSuchElementException()
+//
+//    var max = Int.MinValue
+//    val xsIterator = xs.iterator
+//    while(xsIterator.hasNext){
+//      val currentValue = xsIterator.next()
+//      if(max < currentValue){
+//        max = currentValue
+//      }
+//    }
+//
+//    max
+//  }
 
   def maxRecursive(prevMax: Int, xs: List[Int]): Int = {
-    var res = 0
-    if(xs.isEmpty)return prevMax
-    if(prevMax<  xs.head){
-      res = maxRecursive(xs.head, xs.tail)
+    if(xs.isEmpty){
+      prevMax
+    }else if(prevMax<  xs.head){
+      val res = maxRecursive(xs.head, xs.tail)
+      res
     }else{
-      res = maxRecursive(prevMax, xs.tail)
+      val res = maxRecursive(prevMax, xs.tail)
+      res
     }
-    res
   }
 
   def max(xs: List[Int]):Int ={
